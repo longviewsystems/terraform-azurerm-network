3ec913335917e1e8397222e3695e250c8faaefc3
edward.wang <edward.wang@lvs1.com>
2022-02-02 18:09:21 +0000

    update tf files

--- tf/vnet.tf
+++ tf/vnet.tf
@@ -2,7 +2,8 @@
 module "network" {
   source              = "Azure/network/azurerm"
   vnet_name           = "testvnet"
-  resource_group_name = "azurerm_resource_group.demo.name"
+  #resource_group_name = "azurerm_resource_group.demo.name"
+  resource_group_name = module.naming.resource_group.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
