7e62ff31dbc561f7ed461b94b8416a37680777dd
pradeeponnada <100642832+pradeeponnada@users.noreply.github.com>
2022-03-23 22:38:03 -0600

    Update variables.tf

--- test/fixture/variables.tf
+++ test/fixture/variables.tf
@@ -1,49 +1,3 @@
-/*variable "tags" {
-  description = "Tags to apply to all resources created."
-  type        = map(string)
-}
-
-variable "resource_group_name" {
-  description = "Name of the resource group to be imported."
-  type        = string
-}
-
-variable "vnet_name" {
-  description = "Name of the vnet to create"
-  type        = string
-}
-
-variable "locations" {
-  type        = string
-  description = "The location of the resources."
-}
-
-variable "address_spaces" {
-  type        = list(string)
-  description = "The address space that is used by the virtual network."
-}
-
-variable "dns_servers" {
-  description = "The DNS servers to be used with vNet."
-  type        = list(string)
-  default     = []
-}
-
-variable "subnet_prefixes" {
-  description = "The address prefix to use for the subnet."
-  type        = list(string)
-}
-
-variable "subnet_names" {
-  description = "A list of public subnets inside the vNet."
-  type        = list(string)
-}
-
-variable "subnet_service_endpoints" {
-  description = "A map of subnet name to service endpoints to add to the subnet."
-  type        = map(any)
-}*/
-
 variable "create_resource_group" {
   description = "Whether to create resource group and use it for all networking resources"
   type        = string
@@ -79,8 +33,8 @@ variable "vnet_address_space" {
 
 variable "subnets" {
   description = "For each subnet, create an object that contain fields"
-  type        = list(any)
-  //default     = {}
+  type        = map(any)
+  default     = {}
 }
 
 
