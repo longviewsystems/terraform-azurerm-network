f13fc1c4c43903bd79aaaceb26ed6b83c4141d66
pradeeponnada <100642832+pradeeponnada@users.noreply.github.com>
2022-03-23 22:37:13 -0600

    Update outputs.tf

--- test/fixture/outputs.tf
+++ test/fixture/outputs.tf
@@ -44,27 +44,4 @@ output "network_security_group_ids" {
   value       = [for n in azurerm_network_security_group.nsg : n.id]
 }
 
-output "vnet_id" {
-  description = "The id of the newly created vNet"
-  value       = module.network.vnet_id
-}
 
-output "vnet_name" {
-  description = "The name of the newly created vNet"
-  value       = module.network.vnet_name
-}
-
-output "vnet_location" {
-  description = "The location of the newly created vNet"
-  value       = module.network.vnet_location
-}
-
-output "vnet_address_space" {
-  description = "The address space of the newly created vNet"
-  value       = module.network.vnet_address_space
-}
-
-output "vnet_subnets" {
-  description = "The ids of subnets created inside the newly created vNet"
-  value       = module.network.vnet_subnets
-}
