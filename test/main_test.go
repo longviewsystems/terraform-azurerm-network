9221862ac1a529bd05067db66737d405f909b927
pradeeponnada <100642832+pradeeponnada@users.noreply.github.com>
2022-06-09 11:47:44 -0600

    Update main_test.go

--- test/main_test.go
+++ test/main_test.go
@@ -9,9 +9,24 @@ import (
 
 func TestTerraformAzDoAgent(t *testing.T) {
 	t.Parallel()
+
 	// retryable errors in terraform testing.
 	options := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "./fixture",
+
+
+        Vars: map[string]interface{}{
+			expectedSubnet2 := "subnet02",
+                        expectedSubnet1 := "subnet01",
+                        expectedSubnet3 := "subnet03",
+
+
+			// We also can see how lists and maps translate between terratest and terraform.
+			expectedList2 := []string{expectedSubnet2},
+                        expectedList1 := []string{expectedSubnet1},
+                        expectedList3 := []string{expectedSubnet3},
+		
+		},
 	})
 
 	defer terraform.Destroy(t, options)
@@ -21,8 +36,6 @@ func TestTerraformAzDoAgent(t *testing.T) {
 	output := terraform.Output(t, options, "virtual_network_name1")
 	assert.Equal(t, "vnet-nsg-assc1", output)
 	
-	expectedSubnet1 := "subnet01"
-	expectedList1 := []string{expectedSubnet1}
 	actualSubnetList1 := terraform.OutputList(t, terraformOptions, "subnet_names")
 	assert.Equal(t, expectedList1, actualSubnetList1)
 	
@@ -31,19 +44,16 @@ func TestTerraformAzDoAgent(t *testing.T) {
 	output := terraform.Output(t, options, "virtual_network_name2")
 	assert.Equal(t, "vnet-nsg-assc2", output)
 	
-	expectedSubnet2 := "subnet02"
-	expectedList2 := []string{expectedSubnet2}
 	actualSubnetList2 := terraform.OutputList(t, terraformOptions, "subnet_names")
 	assert.Equal(t, expectedList2, actualSubnetList2)
-	
-	// network-test3//
 
+      	// network-test3//
+	
 	output := terraform.Output(t, options, "virtual_network_name3")
 	assert.Equal(t, "vnet-nsg-assc3", output)
 	
-	expectedSubnet3 := "subnet03"
-	expectedList3 := []string{expectedSubnet3}
 	actualSubnetList3 := terraform.OutputList(t, terraformOptions, "subnet_names")
 	assert.Equal(t, expectedList3, actualSubnetList3)
+	
 
 }
