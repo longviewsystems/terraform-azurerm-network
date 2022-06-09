30f277c7f7eb94b31c13814f01a1859d6af8fdea
pradeeponnada <100642832+pradeeponnada@users.noreply.github.com>
2022-06-08 22:51:00 -0600

    Update outputs.tf

--- test/fixture/outputs.tf
+++ test/fixture/outputs.tf
@@ -1,40 +1,128 @@
+  /* network_test1 outputs **/
+
 
 output "resource_group_name" {
   description = "The name of the resource group in which resources are created"
-  value       = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
-}
-
-output "resource_group_id" {
-  description = "The id of the resource group in which resources are created"
-  value       = element(coalescelist(data.azurerm_resource_group.rgrp.*.id, azurerm_resource_group.rg.*.id, [""]), 0)
-}
-
-output "resource_group_location" {
-  description = "The location of the resource group in which resources are created"
-  value       = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
-}
-
-output "virtual_network_name" {
-  description = "The name of the virtual network"
-  value       = element(concat(azurerm_virtual_network.vnet.*.name, [""]), 0)
-}
-
-output "virtual_network_id" {
-  description = "The id of the virtual network"
-  value       = element(concat(azurerm_virtual_network.vnet.*.id, [""]), 0)
+  value       = module.network_test1.resource_group_name
 }
 
 output "virtual_network_address_space" {
   description = "List of address spaces that are used the virtual network."
-  value       = element(coalescelist(azurerm_virtual_network.vnet.*.address_space, [""]), 0)
+  value       = module.network_test1.virtual_network_address_space
+}
+
+output "resource_group_id" {
+  description = "The id of the resource group in which resources are created"
+  value       = module.network_test1.virtual_network_id
+}
+
+output "resource_group_location" {
+  description = "The location of the resource group in which resources are created"
+  value       = module.network_test1.resource_group_location
+}
+
+output "virtual_network_name" {
+  description = "The name of the virtual network"
+  value       = module.network_test1.virtual_network_name
+}
+
+output "virtual_network_id" {
+  description = "The id of the virtual network"
+  value       = module.network_test1.virtual_network_id
 }
 
 output "subnet_ids" {
   description = "List of IDs of subnets"
-  value       = flatten(concat([for s in azurerm_subnet.snet : s.id]))
+  value       = module.network_test1.subnet_ids
 }
 
 output "network_security_group_ids" {
-  description = "List of Network security groups and ids"
-  value       = [for n in azurerm_network_security_group.nsg : n.id]
+  description = "List of the Network security groups and ids"
+  value       = module.network_test1.network_security_group_ids
 }
+
+  /* network_test2 outputs **/
+
+output "resource_group_name" {
+  description = "The name of the resource group in which resources are created"
+  value       = module.network_test2.resource_group_name
+}
+
+output "virtual_network_address_space" {
+  description = "List of address spaces that are used the virtual network."
+  value       = module.network_test2.virtual_network_address_space
+}
+
+output "resource_group_id" {
+  description = "The id of the resource group in which resources are created"
+  value       = module.network_test2.virtual_network_id
+}
+
+output "resource_group_location" {
+  description = "The location of the resource group in which resources are created"
+  value       = module.network_test2.resource_group_location
+}
+
+output "virtual_network_name" {
+  description = "The name of the virtual network"
+  value       = module.network_test2.virtual_network_name
+}
+
+output "virtual_network_id" {
+  description = "The id of the virtual network"
+  value       = module.network_test2.virtual_network_id
+}
+
+output "subnet_ids" {
+  description = "List of IDs of subnets"
+  value       = module.network_test2.subnet_ids
+}
+
+output "network_security_group_ids" {
+  description = "List of the Network security groups and ids"
+  value       = module.network_test2.network_security_group_ids
+}
+  
+    /* network_test3 outputs **/
+  
+  
+output "resource_group_name" {
+  description = "The name of the resource group in which resources are created"
+  value       = module.network_test3.resource_group_name
+}
+
+output "virtual_network_address_space" {
+  description = "List of address spaces that are used the virtual network."
+  value       = module.network_test3.virtual_network_address_space
+}
+
+output "resource_group_id" {
+  description = "The id of the resource group in which resources are created"
+  value       = module.network_test3.virtual_network_id
+}
+
+output "resource_group_location" {
+  description = "The location of the resource group in which resources are created"
+  value       = module.network_test3.resource_group_location
+}
+
+output "virtual_network_name" {
+  description = "The name of the virtual network"
+  value       = module.network_test3.virtual_network_name
+}
+
+output "virtual_network_id" {
+  description = "The id of the virtual network"
+  value       = module.network_test3.virtual_network_id
+}
+
+output "subnet_ids" {
+  description = "List of IDs of subnets"
+  value       = module.network_test3.subnet_ids
+}
+
+output "network_security_group_ids" {
+  description = "List of the Network security groups and ids"
+  value       = module.network_test3.network_security_group_ids
+}
+  
