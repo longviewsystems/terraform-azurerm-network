e6778223033a139869c127aca56c5a0e71331a3e
pradeeponnada <100642832+pradeeponnada@users.noreply.github.com>
2022-06-08 23:49:52 -0600

    Update outputs.tf

--- test/fixture/outputs.tf
+++ test/fixture/outputs.tf
@@ -35,6 +35,11 @@ output "subnet_ids1" {
   description = "List of IDs of subnets"
   value       = module.network_test1.subnet_ids
 }
+  
+output "subnet_names1" {
+  description = "List of names of subnets"
+  value       = module.network_test1.subnet_names
+}
 
 output "network_security_group_ids1" {
   description = "List of the Network security groups and ids"
@@ -76,6 +81,11 @@ output "virtual_network_id2" {
 output "subnet_ids2" {
   description = "List of IDs of subnets"
   value       = module.network_test2.subnet_ids
+}
+  
+output "subnet_names2" {
+  description = "List of names of subnets"
+  value       = module.network_test2.subnet_names
 }
 
 output "network_security_group_ids2" {
@@ -119,6 +129,11 @@ output "virtual_network_id3" {
 output "subnet_ids3" {
   description = "List of IDs of subnets"
   value       = module.network_test3.subnet_ids
+}
+  
+output "subnet_names3" {
+  description = "List of names of subnets"
+  value       = module.network_test3.subnet_names
 }
 
 output "network_security_group_ids3" {
