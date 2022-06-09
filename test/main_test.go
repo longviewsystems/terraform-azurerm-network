ddc71f3c86046eeeb56bfc69959897e1db0638a0
pradeeponnada <100642832+pradeeponnada@users.noreply.github.com>
2022-06-09 12:27:04 -0600

    Update main_test.go

--- test/main_test.go
+++ test/main_test.go
@@ -1,61 +1,41 @@
 package main
 
 import (
+	"log"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestTerraformAzDoAgent(t *testing.T) {
-	t.Parallel()
-
-	// retryable errors in terraform testing.
-	options := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "./fixture",
-
-
-        Vars: map[string]interface{}{
-			"expectedSubnet1": subnet01,
-                        "expectedSubnet2": subnet02,
-                        "expectedSubnet3": subnet03,
-
-			// We also can see how lists and maps translate between terratest and terraform.
-			"expectedList1": []string{expectedSubnet1},
-                        "expectedList2": []string{expectedSubnet2},
-                        "expectedList3": []string{expectedSubnet3},
-		
-		},
-		
-		// Variables to pass to our Terraform code using -var-file options
-		VarFiles: []string{"terraform.tfvars"},
-	})
+	options := &terraform.Options{
+		TerraformDir: "../test/fixture",
+		VarFiles:     []string{"../../terraform.tfvars"},
+	}
 
 	defer terraform.Destroy(t, options)
-	
-	terraform.InitAndApply(t, options)
-	
-	output := terraform.Output(t, options, "virtual_network_name1")
-	assert.Equal(t, "vnet-nsg-assc1", output)
-	
-	actualSubnetList1 := terraform.OutputList(t, terraformOptions, "subnet_names")
-	assert.Equal(t, expectedList1, actualSubnetList1)
-	
-	// network-test2//
-	
-	output := terraform.Output(t, options, "virtual_network_name2")
-	assert.Equal(t, "vnet-nsg-assc2", output)
-	
-	actualSubnetList2 := terraform.OutputList(t, terraformOptions, "subnet_names")
-	assert.Equal(t, expectedList2, actualSubnetList2)
 
-      	// network-test3//
-	
-	output := terraform.Output(t, options, "virtual_network_name3")
-	assert.Equal(t, "vnet-nsg-assc3", output)
-	
-	actualSubnetList3 := terraform.OutputList(t, terraformOptions, "subnet_names")
-	assert.Equal(t, expectedList3, actualSubnetList3)
-	
+	init, err := terraform.InitE(t, options)
 
+	if err != nil {
+		log.Println(err)
+	}
+
+	t.Log(init)
+
+	plan, err := terraform.PlanE(t, options)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	t.Log(plan)
+
+	apply, err := terraform.ApplyE(t, options)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	t.Log(apply)
 }
