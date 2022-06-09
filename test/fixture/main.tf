125c53525c5feffa3b658ef75573f64e86a4b3b6
pradeeponnada <100642832+pradeeponnada@users.noreply.github.com>
2022-06-08 22:41:56 -0600

    Update main.tf

--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -1,130 +1,133 @@
-#------------------------
-# Local declarations
-#------------------------
+module "network_test1" {
+source              = "../../"
+resource_group_name = "vnet-nsg"
+vnetwork_name       = "vnet-nsg-assc"
+location            = "EastUS"
+vnet_address_space  = ["10.1.0.0/16"]
+dns_servers         = ["10.1.1.24"]
 
-resource "random_string" "random" {
-  length           = 4
-  special          = false
+
+subnets = {
+  mgnt_subnet = {
+    subnet_name                                    = "subnet01"
+    subnet_address_prefix                          = ["10.1.2.0/24"]
+    create_nsg                                     = true
+    nsg_name                                       = "NSG-Subnet01"
+    enforce_private_link_endpoint_network_policies = true
+    service_endpoints                              = ["Microsoft.Storage"]
+    nsg_inbound_rules = [
+      # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+      # To use defaults, use "" without adding any values.
+      ["weballow", "100", "Inbound", "Allow", "Tcp", "80", "*", "0.0.0.0/0"],
+      ["weballow1", "101", "Inbound", "Allow", "", "443", "*", ""],
+      ["weballow2", "102", "Inbound", "Allow", "Tcp", "8080-8090", "*", ""],
+    ]
+
+    nsg_outbound_rules = [
+      # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+      # To use defaults, use "" without adding any values.
+      ["ntp_out", "103", "Outbound", "Allow", "Udp", "123", "", "0.0.0.0/0"],
+    ]
+    
+    create_route_table            = true
+    route_table_name              = "Routetable-Subnet01"
+    disable_bgp_route_propagation = true
+    routes=[
+      #[route_name,address_prefix,next_hop_type,next_hop_in_ip_address]
+      ["default","0.0.0.0/0","Internet",null],
+      ["AzureFireWall","10.0.2.0/24","VnetLocal",null]
+    ]
+  }
+
 }
-
-locals {
-  resource_group_name = "rg-${var.resource_group_name}-${random_string.random.result}"
-  location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
 }
-
-
-data "azurerm_resource_group" "rgrp" {
-  count = var.create_resource_group == false ? 1 : 0
-  name  = local.resource_group_name
-}
-
-
-resource "azurerm_resource_group" "rg" {
-  count    = var.create_resource_group ? 1 : 0
-  name     = local.resource_group_name
-  location = var.location
-  tags     = merge({ "Name" = format("%s", var.resource_group_name) }, var.tags, )
-}
-
-#-------------------------------------
-# VNET Creation - Default is "true"
-#-------------------------------------
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnetwork_name
-  location            = local.location
-  resource_group_name = local.resource_group_name
-  dns_servers         = var.dns_servers
-  address_space       = var.vnet_address_space
-  tags                = merge({ "Name" = format("%s", var.vnetwork_name) }, var.tags, )
-
-}
-
-#--------------------------------------------------------------------------------------------------------
-# Subnets Creation with, NSG Association
-#--------------------------------------------------------------------------------------------------------
-
-
-resource "azurerm_subnet" "snet" {
-  for_each                                       = var.subnets
-  name                                           = each.value.subnet_name
-  resource_group_name                            = local.resource_group_name
-  virtual_network_name                           = azurerm_virtual_network.vnet.name
-  address_prefixes                               = each.value.subnet_address_prefix
-  service_endpoints                              = lookup(each.value, "service_endpoints", [])
-  enforce_private_link_endpoint_network_policies = lookup(each.value, "enforce_private_link_endpoint_network_policies", null)
   
-}
+module "network_test2" {
+source              = "../../"
+resource_group_name = "vnet-nsg"
+vnetwork_name       = "vnet-nsg-assc"
+location            = "EastUS"
+vnet_address_space  = ["10.1.0.0/16"]
+dns_servers         = ["10.1.1.24"]
 
-#-----------------------------------------------
-# Network security group - Default is "false"
-#-----------------------------------------------
-resource "azurerm_network_security_group" "nsg" {
-  for_each = {
-    for name, subnets in var.subnets : name => subnets
-    if subnets.create_nsg == true
-  }
-  name                = each.value["nsg_name"]
-  resource_group_name = local.resource_group_name
-  location            = local.location
-  tags                = merge({ "ResourceName" = lower("nsg_${each.key}_in") }, var.tags, )
-  dynamic "security_rule" {
-    for_each = concat(lookup(each.value, "nsg_inbound_rules", []), lookup(each.value, "nsg_outbound_rules", []))
-    content {
-      name                       = security_rule.value[0] == "" ? "Default_Rule" : security_rule.value[0]
-      priority                   = security_rule.value[1]
-      direction                  = security_rule.value[2] == "" ? "Inbound" : security_rule.value[2]
-      access                     = security_rule.value[3] == "" ? "Allow" : security_rule.value[3]
-      protocol                   = security_rule.value[4] == "" ? "Tcp" : security_rule.value[4]
-      source_port_range          = "*"
-      destination_port_range     = security_rule.value[5] == "" ? "*" : security_rule.value[5]
-      source_address_prefix      = security_rule.value[6] == "" ? element(each.value.subnet_address_prefix, 0) : security_rule.value[6]
-      destination_address_prefix = security_rule.value[7] == "" ? element(each.value.subnet_address_prefix, 0) : security_rule.value[7]
-      description                = "${security_rule.value[2]}_Port_${security_rule.value[5]}"
-    }
-  }
-}
-
-resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
-  for_each = {
-    for name, subnets in var.subnets : name => subnets
-    if subnets.create_nsg == true
-  }
-  subnet_id                 = azurerm_subnet.snet[each.key].id
-  network_security_group_id = azurerm_network_security_group.nsg[each.key].id
-}
-    
-#-----------------------------------------------
-#          Route Tables 
-#-----------------------------------------------
-    
-resource "azurerm_route_table" "route_table" {
-  for_each = {
-    for name, subnets in var.subnets : name => subnets
-    if subnets.create_route_table == true
-  }
-  name                          = each.value.route_table_name
-  location                      = var.location
-  resource_group_name           = var.resource_group_name
-  disable_bgp_route_propagation = each.value.disable_bgp_route_propagation
-  tags                          = var.tags
+subnets = {
   
-  dynamic "route" {
-    for_each = each.value.routes
-    content {
-      name                   = route.value[0] == "" ? "default" : route.value[0]
-      address_prefix         = route.value[1] 
-      next_hop_type          = route.value[2]
-      next_hop_in_ip_address = route.value[3]
-    }
-  }
-}
+  mgnt_subnet2 = {
+    subnet_name                                    = "subnet02"
+    subnet_address_prefix                          = ["10.1.3.0/24"]
+    create_nsg                                     = true
+    nsg_name                                       = "NSG-Subnet02"
+    service_endpoints                              = ["Microsoft.Sql"]
+    enforce_private_link_endpoint_network_policies = true
+    nsg_inbound_rules = [
+      # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+      # To use defaults, use "" without adding any values.
+      ["weballow", "100", "Inbound", "Allow", "Tcp", "80", "*", "0.0.0.0/0"],
+      ["weballow1", "101", "Inbound", "Allow", "", "443", "*", ""]
+    ]
+
+    nsg_outbound_rules = [
+      # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+      # To use defaults, use "" without adding any values.
+      ["ntp_out", "103", "Outbound", "Allow", "Udp", "123", "", "0.0.0.0/0"],
+    ]
     
-resource "azurerm_subnet_route_table_association" "routetable" {
-   for_each = {
-    for name, subnets in var.subnets : name => subnets
-    if subnets.create_route_table == true
+    create_route_table            = true
+    route_table_name              = "Routetable-Subnet02"
+    disable_bgp_route_propagation = true
+    routes=[
+      #[route_name,address_prefix,next_hop_type,next_hop_in_ip_address]
+      ["default","0.0.0.0/0","Internet",null],
+      ["AzureFireWall","10.0.2.0/24","VnetLocal",null]
+    ]
   }
-  subnet_id                 = azurerm_subnet.snet[each.key].id
-  route_table_id            = azurerm_route_table.route_table[each.key].id
 }
+}
+module "network_test3" {
+source              = "../../"
+resource_group_name = "vnet-nsg"
+vnetwork_name       = "vnet-nsg-assc"
+location            = "EastUS"
+vnet_address_space  = ["10.1.0.0/16"]
+dns_servers         = ["10.1.1.24"]
+
+subnets = {
+mgnt_subnet3 = {
+    subnet_name                                    = "subnet03"
+    subnet_address_prefix                          = ["10.1.4.0/24"]
+    create_nsg                                     = false
+    nsg_name                                       = ""
+    service_endpoints                              = ["Microsoft.Sql"]
+    enforce_private_link_endpoint_network_policies = true
+    nsg_inbound_rules = [
+
+      # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+
+      # To use defaults, use "" without adding any values.
+
+      ["", "", "", "", "", "", "", ""],
+
+    ]
+
+    nsg_outbound_rules = [
+
+      # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+
+      # To use defaults, use "" without adding any values.
+
+      ["", "", "", "", "", "", "", ""],
+
+    ]
+    
+    create_route_table            = false
+    route_table_name              = "default"
+    disable_bgp_route_propagation = true
+    routes=[
+      #[route_name,address_prefix,next_hop_type,next_hop_in_ip_address]
+      ["","","",null]
+    ]
+  }
+}
+}
+
+  
