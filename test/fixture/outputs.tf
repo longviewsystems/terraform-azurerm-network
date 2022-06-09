ebfc23ba31dc5bedcc64486a349c9c3bf390dc37
pradeeponnada <100642832+pradeeponnada@users.noreply.github.com>
2022-06-08 23:02:11 -0600

    Update outputs.tf

--- test/fixture/outputs.tf
+++ test/fixture/outputs.tf
@@ -1,84 +1,84 @@
   /* network_test1 outputs **/
 
 
-output "resource_group_name" {
+output "resource_group_name1" {
   description = "The name of the resource group in which resources are created"
   value       = module.network_test1.resource_group_name
 }
 
-output "virtual_network_address_space" {
+output "virtual_network_address_space1" {
   description = "List of address spaces that are used the virtual network."
   value       = module.network_test1.virtual_network_address_space
 }
 
-output "resource_group_id" {
+output "resource_group_id1" {
   description = "The id of the resource group in which resources are created"
   value       = module.network_test1.virtual_network_id
 }
 
-output "resource_group_location" {
+output "resource_group_location1" {
   description = "The location of the resource group in which resources are created"
   value       = module.network_test1.resource_group_location
 }
 
-output "virtual_network_name" {
+output "virtual_network_name1" {
   description = "The name of the virtual network"
   value       = module.network_test1.virtual_network_name
 }
 
-output "virtual_network_id" {
+output "virtual_network_id1" {
   description = "The id of the virtual network"
   value       = module.network_test1.virtual_network_id
 }
 
-output "subnet_ids" {
+output "subnet_ids1" {
   description = "List of IDs of subnets"
   value       = module.network_test1.subnet_ids
 }
 
-output "network_security_group_ids" {
+output "network_security_group_ids1" {
   description = "List of the Network security groups and ids"
   value       = module.network_test1.network_security_group_ids
 }
 
   /* network_test2 outputs **/
 
-output "resource_group_name" {
+output "resource_group_name2" {
   description = "The name of the resource group in which resources are created"
   value       = module.network_test2.resource_group_name
 }
 
-output "virtual_network_address_space" {
+output "virtual_network_address_space2" {
   description = "List of address spaces that are used the virtual network."
   value       = module.network_test2.virtual_network_address_space
 }
 
-output "resource_group_id" {
+output "resource_group_id2" {
   description = "The id of the resource group in which resources are created"
   value       = module.network_test2.virtual_network_id
 }
 
-output "resource_group_location" {
+output "resource_group_location2" {
   description = "The location of the resource group in which resources are created"
   value       = module.network_test2.resource_group_location
 }
 
-output "virtual_network_name" {
+output "virtual_network_name2" {
   description = "The name of the virtual network"
   value       = module.network_test2.virtual_network_name
 }
 
-output "virtual_network_id" {
+output "virtual_network_id2" {
   description = "The id of the virtual network"
   value       = module.network_test2.virtual_network_id
 }
 
-output "subnet_ids" {
+output "subnet_ids2" {
   description = "List of IDs of subnets"
   value       = module.network_test2.subnet_ids
 }
 
-output "network_security_group_ids" {
+output "network_security_group_ids2" {
   description = "List of the Network security groups and ids"
   value       = module.network_test2.network_security_group_ids
 }
@@ -86,42 +86,42 @@ output "network_security_group_ids" {
     /* network_test3 outputs **/
   
   
-output "resource_group_name" {
+output "resource_group_name3" {
   description = "The name of the resource group in which resources are created"
   value       = module.network_test3.resource_group_name
 }
 
-output "virtual_network_address_space" {
+output "virtual_network_address_space3" {
   description = "List of address spaces that are used the virtual network."
   value       = module.network_test3.virtual_network_address_space
 }
 
-output "resource_group_id" {
+output "resource_group_id3" {
   description = "The id of the resource group in which resources are created"
   value       = module.network_test3.virtual_network_id
 }
 
-output "resource_group_location" {
+output "resource_group_location3" {
   description = "The location of the resource group in which resources are created"
   value       = module.network_test3.resource_group_location
 }
 
-output "virtual_network_name" {
+output "virtual_network_name3" {
   description = "The name of the virtual network"
   value       = module.network_test3.virtual_network_name
 }
 
-output "virtual_network_id" {
+output "virtual_network_id3" {
   description = "The id of the virtual network"
   value       = module.network_test3.virtual_network_id
 }
 
-output "subnet_ids" {
+output "subnet_ids3" {
   description = "List of IDs of subnets"
   value       = module.network_test3.subnet_ids
 }
 
-output "network_security_group_ids" {
+output "network_security_group_ids3" {
   description = "List of the Network security groups and ids"
   value       = module.network_test3.network_security_group_ids
 }
