bba273dea14d43f6f5d92c3b216ace348a567862
craig.holmes <craig.holmes@lvs1.com>
2022-06-22 18:41:24 +0000

    refactor: move nsg to new file

--- main.tf
+++ main.tf
@@ -29,44 +29,6 @@ resource "azurerm_subnet" "snet" {
 }
 
 #-----------------------------------------------
-# Network security group - Default is "false"
-#-----------------------------------------------
-resource "azurerm_network_security_group" "nsg" {
-  for_each = {
-    for name, subnets in var.subnets : name => subnets
-    if subnets.create_nsg == true
-  }
-  name                = each.value["nsg_name"]
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  tags                = merge({ "ResourceName" = lower("nsg_${each.key}_in") }, var.tags, )
-  dynamic "security_rule" {
-    for_each = concat(lookup(each.value, "nsg_inbound_rules", []), lookup(each.value, "nsg_outbound_rules", []))
-    content {
-      name                       = security_rule.value[0] == "" ? "Default_Rule" : security_rule.value[0]
-      priority                   = security_rule.value[1]
-      direction                  = security_rule.value[2] == "" ? "Inbound" : security_rule.value[2]
-      access                     = security_rule.value[3] == "" ? "Allow" : security_rule.value[3]
-      protocol                   = security_rule.value[4] == "" ? "Tcp" : security_rule.value[4]
-      source_port_range          = "*"
-      destination_port_range     = security_rule.value[5] == "" ? "*" : security_rule.value[5]
-      source_address_prefix      = security_rule.value[6] == "" ? element(each.value.subnet_address_prefix, 0) : security_rule.value[6]
-      destination_address_prefix = security_rule.value[7] == "" ? element(each.value.subnet_address_prefix, 0) : security_rule.value[7]
-      description                = "${security_rule.value[2]}_Port_${security_rule.value[5]}"
-    }
-  }
-}
-
-resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
-  for_each = {
-    for name, subnets in var.subnets : name => subnets
-    if subnets.create_nsg == true
-  }
-  subnet_id                 = azurerm_subnet.snet[each.key].id
-  network_security_group_id = azurerm_network_security_group.nsg[each.key].id
-}
-
-#-----------------------------------------------
 #          Route Tables 
 #-----------------------------------------------
     
