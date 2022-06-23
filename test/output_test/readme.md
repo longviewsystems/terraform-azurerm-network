ef8e6da3e92ce3bf552b1f5beddc0559b8bccc81
craig.holmes <craig.holmes@lvs1.com>
2022-06-23 22:06:25 +0000

    feat: test outputs

---
+++ test/output_test/readme.md
@@ -0,0 +1,11 @@
+# Overview 
+The subnets variable is complex.  This folder is to help test expressions on it.  Please leave previous samples intact.
+
+# Usage
+Run normal terraform commands
+
+```
+  terraform init
+  terraform plan
+  terraform apply
+```
---
+++ test/output_test/sample2.tf
@@ -0,0 +1,56 @@
+locals {
+  subnets = {
+    AzureFirewall = {
+      subnet_name                                    = "AzureFirewall"
+      subnet_address_prefix                          = ["10.1.2.0/24"]
+      create_nsg                                     = true
+      nsg_name                                       = "NSG-Subnet01"
+      route_table_id = "/subscriptions/57215661-2f9e-482f-9334-c092e02651ec/resourceGroups/mod-test1-rg-lqfs/providers/Microsoft.Network/routeTables/rt-test1-other"
+      enforce_private_link_endpoint_network_policies = true
+      service_endpoints                              = ["Microsoft.Storage"]
+      nsg_inbound_rules = [
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+        # To use defaults, use "" without adding any values.
+        ["weballow", "100", "Inbound", "Allow", "Tcp", "80", "*", "0.0.0.0/0"],
+        ["weballow1", "101", "Inbound", "Allow", "", "443", "*", ""],
+        ["weballow2", "102", "Inbound", "Allow", "Tcp", "8080-8090", "*", ""],
+      ]
+
+      nsg_outbound_rules = [
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+        # To use defaults, use "" without adding any values.
+        ["ntp_out", "103", "Outbound", "Allow", "Udp", "123", "", "0.0.0.0/0"],
+      ]
+    }
+    app_sn = {
+      subnet_name                                    = "app_sn"
+      subnet_address_prefix                          = ["10.1.2.0/24"]
+      create_nsg                                     = true
+      nsg_name                                       = "NSG-Subnet01"
+      route_table_id = "/subscriptions/57215661-2f9e-482f-9334-c092e02651ec/resourceGroups/mod-test1-rg-lqfs/providers/Microsoft.Network/routeTables/rt-test1-other"
+      enforce_private_link_endpoint_network_policies = true
+      service_endpoints                              = ["Microsoft.Storage"]
+      nsg_inbound_rules = [
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+        # To use defaults, use "" without adding any values.
+        ["weballow", "100", "Inbound", "Allow", "Tcp", "80", "*", "0.0.0.0/0"],
+        ["weballow1", "101", "Inbound", "Allow", "", "443", "*", ""],
+        ["weballow2", "102", "Inbound", "Allow", "Tcp", "8080-8090", "*", ""],
+      ]
+
+      nsg_outbound_rules = [
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+        # To use defaults, use "" without adding any values.
+        ["ntp_out", "103", "Outbound", "Allow", "Udp", "123", "", "0.0.0.0/0"],
+      ]
+    }
+  }
+}
+
+output "route_table_ids" {
+  value = {
+    for key, subnets in local.subnets : key => subnets
+    if subnets.route_table_id != null && subnets.route_table_id != "" 
+  }
+}
+
