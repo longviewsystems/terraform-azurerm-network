b24b7167075c4d542905e1875b7d7c5b17e32c47
Giridhar Kolli <Giridhar.Kolli@lvs1.com>
2023-04-27 12:11:30 -0700

    feat: updated locals.tf

--- locals.tf
+++ locals.tf
@@ -2,6 +2,6 @@ locals {
   #Build the list that will be used to map route_tables to subnets
   route_table_list = {
     for name, subnets in var.subnets : name => subnets
-    if try(subnets.route_table_id, null) != null && subnets.route_table_id != ""
+    if subnets.route_table_rg_name != null && subnets.route_table_rg_name != "" && subnets.route_table_name != null && subnets.route_table_name != ""
   }
 }
